9d9abe7e7723fd46e8bb98387f59d4ba34f972dc
Rio358 <56245429+Rio358@users.noreply.github.com>
2021-06-23 15:56:34 -0400

    Create bank.sql

---
+++ banking/bank.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `transactions` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`identifier` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`type` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`amount` INT(11) NULL DEFAULT NULL,
+	`date` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=182
+;
